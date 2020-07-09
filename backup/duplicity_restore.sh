01961288ff04f295bb71319a10b617063b4fd7f3
altintasali <altintas.ali@gmail.com>
2020-07-09 16:38:44 +0200

    restore file/folder using duplicity backup

--- backup/duplicity_restore.sh
+++ backup/duplicity_restore.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# USAGE: ./duplicity_restore.sh [FILE/FOLDER deleted (full path)] [RESTORE directory]
 # Currently experimental, use with proper caution
-
+ 
 RESTORE_ME=$(readlink -f $1)
 RESTORE_ME2=$(echo $RESTORE_ME | awk '{print substr($1,2)}')
 RESTORE_BASE=$(echo $RESTORE_ME | awk -F "/" '{print $NF}')
@@ -9,7 +10,8 @@ RESTORE_BASE=$(echo $RESTORE_ME | awk -F "/" '{print $NF}')
 DUPLICITY_BACK="file:///media/Backup/Data/"
 DIROUT=$2/$RESTORE_BASE
 
-duplicity --no-encryption \
+duplicity restore \
 	  --file-to-restore $RESTORE_ME2 \
+	  --no-encryption \
 	  $DUPLICITY_BACK $DIROUT
 
