9b44f1bac7df2fc8da1c0feca69e581c962a4737
altintasali <altintas.ali@gmail.com>
2020-07-09 15:59:31 +0200

    restore file/folder using duplicity backup

--- backup/duplicity_restore.sh
+++ backup/duplicity_restore.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+# Currently experimental, use with proper caution
+
 RESTORE_ME=$(readlink -f $1)
 RESTORE_ME2=$(echo $RESTORE_ME | awk '{print substr($1,2)}')
+RESTORE_BASE=$(echo $RESTORE_ME | awk -F "/" '{print $NF}')
 
 DUPLICITY_BACK="file:///media/Backup/Data/"
-DIROUT=$2
+DIROUT=$2/$RESTORE_BASE
 
 duplicity --no-encryption \
 	  --file-to-restore $RESTORE_ME2 \
